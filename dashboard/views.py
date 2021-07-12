72a448c46f8f3a868f49c32ee9ac5c5c2158d671
Duy Ngo <duy.ngo94@gmail.com>
2021-07-11 18:07:42 -0700

    resident login

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,17 +3,19 @@ from django.template import loader
 
 # Create your views here.
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
-# Create your views here.
 from django.http import HttpResponse, JsonResponse
 import simplejson as json
 
 user = {}
+dynamodb = boto3.resource('dynamodb')
+
 
 def dashboard(request):
     global user
-    dynamodb = boto3.resource('dynamodb')
-    user = dynamodb.Table('Renter').get_item(Key={'renter_id':1, 'property_id':1})
+    global dynamodb
+    #user = dynamodb.Table('Renter').get_item(Key={'renter_id':1, 'property_id':1})
     #user = json.dumps(dynamodb.Table('Renter').get_item(Key={'renter_id':1, 'property_id':1}))
 
     #print(user)
@@ -43,9 +45,22 @@ def info(request):
     return render(request, 'portal/info.html')
 
 def index(request):
+    global user
     if request.method == 'POST':
+        uname = request.POST['uname']
+        psw = request.POST['psw']
         print(request.POST['uname'])
         print(request.POST['psw'])
-        return redirect('/portal')
+        scanres = dynamodb.Table('Renter').scan(
+            FilterExpression=Attr('renter_username').eq(uname) & Attr('renter_password').eq(psw)
+            )
+        if len(scanres['Items']) == 0:
+            # reload page if login fail, add fail message later
+            return render(request, 'portal/index.html')
+        else: 
+            #expect only one user & password pair so index [0] for user.
+            user = scanres['Items'][0]
+            print(user)
+            return redirect('/portal')
     else:
         return render(request, 'portal/index.html')
